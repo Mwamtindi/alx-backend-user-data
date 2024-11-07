1726003b4217a3e624f607614128d055e65d2b6a
Mwamtindi <mwamtindishaban@gmail.com>
2024-11-07 20:30:36 +0300

    Log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,6 +3,7 @@
 Module with fxn filter_datum that filters sensitive info from log messages.
 """
 
+import logging
 import re
 from typing import List
 
@@ -23,3 +24,36 @@ def filter_datum(fields: List[str], redaction: str,
     """
     pattern = f"({'|'.join(fields)})=([^ {separator}]*)"
     return re.sub(pattern, lambda m: f"{m.group(1)}={redaction}", message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """
+        Initialize the formatter with given fields to redact.
+
+        Args:
+            fields: List of field names to obfuscate.
+        """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        Format log record, redacting sensitive info from specified fields.
+
+        Args:
+            record: The log record to format.
+
+        Returns:
+            The formatted log message with redacted sensitive fields.
+        """
+        record.msg = filter_datum(self.fields, self.REDACTION,
+                                  record.getMessage(), self.SEPARATOR)
+        return super().format(record)
