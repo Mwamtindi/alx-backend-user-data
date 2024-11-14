3c07b1a40b61f4e375cce9fe85fc290d2f572016
Mwamtindi <mwamtindishaban@gmail.com>
2024-11-14 21:55:12 +0300

    basic auth

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+BasicAuth module that inherits from Auth.
+"""
+
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """Basic Authentication class that inherits from Auth."""
+    pass
