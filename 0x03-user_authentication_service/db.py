e93c0ff800cb93b40e051adfbf5e8bb8df124c76
Mwamtindi <mwamtindishaban@gmail.com>
2024-11-21 20:47:02 +0300

    find user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -9,7 +9,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 from user import Base, User
 
-from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.exc import InvalidRequestError, NoResultFound
 from sqlalchemy.orm.exc import NoResultFound
 
 
@@ -64,20 +64,15 @@ class DB:
             NoResultFound: If no user matches the query.
             InvalidRequestError: If query arguments are invalid.
         """
-        if not kwargs:
-            raise InvalidRequestError
-
-        columns = User.__table__.columns.keys()
         for key in kwargs.keys():
-            if key not in columns:
-                raise InvalidRequestError
+            if not hasattr(User, key):
+                raise InvalidRequestError()
 
         user = self._session.query(User).filter_by(**kwargs).first()
 
-        if user is None:
-            raise NoResultFound
-
-        return user
+        if user:
+            return user
+        raise NoResultFound()
 
     def update_user(self, user_id: int, **kwargs) -> None:
         """
